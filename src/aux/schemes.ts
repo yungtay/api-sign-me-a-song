84f65daba9dd86be462e836fcd3d550ba3047759
leleo <celso@respondeai.com.br>
2021-08-03 17:21:52 -0300

    code review

--- src/aux/schemes.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi'
-
-const schemeRecommendationName = Joi.string().min(1).required()
-const schemeYoutube = Joi.string().pattern(new RegExp('^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$'))
-
-export {schemeRecommendationName, schemeYoutube}
